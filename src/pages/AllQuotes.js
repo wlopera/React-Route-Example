639e52d41695ef847b262bc115630226996be67f
lopera.william@gmail.com <William>
2022-04-05 13:44:24 -0500

    Agregar manejo de rutas

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,7 +1,21 @@
 import React from "react";
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  {
+    id: "q1",
+    author: "William Lopera",
+    text: "Aprender React es divertido!",
+  },
+  {
+    id: "q2",
+    author: "Andrés Lopera",
+    text: "Aprender React es grandioso!",
+  },
+];
 
 const AllQuotes = () => {
-  return <div>AllQuotes</div>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
--- src/pages/NewQuotes.js
+++ src/pages/NewQuotes.js
@@ -1,7 +1,12 @@
 import React from "react";
+import QuoteForm from "../components/quotes/QuoteForm";
 
 const NewQuotes = () => {
-  return <div>NewQuotes</div>;
+  const addQuoteHandler = (quoteData) => {
+    console.log("Agregar cita:", quoteData);
+  };
+
+  return <QuoteForm onAddQuote={addQuoteHandler} />;
 };
 
 export default NewQuotes;
